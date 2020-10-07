c5f3eb9f379cc1373977bf0743e5e592fd5734e3
Chris Lenart <clenart4@kent.edu>
2020-10-06 21:08:01 -0400

    added debug  for all non prod urls

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,22 @@ from app import home
 
 # are we executing at PythonAnywhere?
 ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ
+ON_PROD = ON_PYTHONANYWHERE and os.environ["PYTHONANYWHERE_DOMAIN"] == "webprod-chrisdesigns.pythonanywhere.com"
 
 if ON_PYTHONANYWHERE:
     # on PA, set up to connect to the WSGI server
     from bottle import default_app
 else:
     # on the development environment, import the development server
-    from bottle import run, debug
+    from bottle import run
 
+# set debug if not on prod (could be dev on PA)
+if not ON_PROD:
+    from bottle import debug
+    debug(True)
 
 if ON_PYTHONANYWHERE:
     # on PA, connect to the WSGI server
     application = default_app()
 else:
-    # on the development environment, run the development server
-    debug(True)
     run(host='localhost', port=8080)
